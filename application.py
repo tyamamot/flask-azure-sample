9b34074f84eae07a5daeb171dbc05b3013a0f48c
Takehiro Yamamoto <t.yamamoto@sis.u-hyogo.ac.jp>
2021-07-01 11:02:09 +0900

    my first flask

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/search')
+def search():
+    return app.send_static_file('search.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, threaded=True)  
